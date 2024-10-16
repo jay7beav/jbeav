44e33b069b195b791246c2b8c5b02ef549c6bc5c
jay7beav <jbeavers@samford.edu>
2024-10-16 12:52:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function downloadData() {
   event.preventDefault();
   var startdate = document.getElementById("startdate").value;
   var enddate = document.getElementById("enddate").value;
-  alert(startdate);
-  alert(enddate);
+  console.log("Starting date: " + startdate);
+  console.log("Ending date: " + enddate);
+  fetch("https://compute.samford.edu/zohauth/clients/data");
 }
